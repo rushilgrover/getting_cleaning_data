6c3b4889d53e76950572dbc342d7ab4ef929626c
Rushil Grover <rushil.grover15@gmail.com>
2014-10-26 15:50:49 +0530

    Changes to the logical flow of the code & edited some comments

--- run_analysis.R
+++ run_analysis.R
@@ -3,15 +3,15 @@
 # author: Rushil Grover
 # date: October,2014
 
-# runAnalysis.r File Description:
+# runAnalysis.r Description:
 
 # This script will perform the following steps on the UCI HAR Dataset downloaded from 
 # https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip 
-# 1. Merge the training and the test sets to create one data set.
-# 2. Extract only the measurements on the mean and standard deviation for each measurement. 
-# 3. Use descriptive activity names to name the activities in the data set
-# 4. Appropriately label the data set with descriptive activity names. 
-# 5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
+# 1. Merges the training and the test sets to create one data set.
+# 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+# 3. Uses descriptive activity names to name the activities in the data set
+# 4. Appropriately labels the data set with descriptive variable names. 
+# 5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
 ##########################################################################################################
 
@@ -19,19 +19,29 @@
 # Clean up workspace
 rm(list=ls())
 
-# 1. Merge the training and the test sets to create one data set.
+# 1. Merges the training and the test sets to create one data set.
 
 #set working directory to the location where the UCI HAR Dataset was unzipped
 setwd('/Users/rushil/Getting_and_Cleaning_Data/UCI HAR Dataset/');
 
-# Read in the data from files
+# Read in the training data from files
 features     = read.table('./features.txt',header=FALSE); #imports features.txt
 activityType = read.table('./activity_labels.txt',header=FALSE); #imports activity_labels.txt
 subjectTrain = read.table('./train/subject_train.txt',header=FALSE); #imports subject_train.txt
 xTrain       = read.table('./train/x_train.txt',header=FALSE); #imports x_train.txt
 yTrain       = read.table('./train/y_train.txt',header=FALSE); #imports y_train.txt
 
-# Assigin column names to the data imported above
+# Read in the test data from files
+subjectTest = read.table('./test/subject_test.txt',header=FALSE); #imports subject_test.txt
+xTest       = read.table('./test/x_test.txt',header=FALSE); #imports x_test.txt
+yTest       = read.table('./test/y_test.txt',header=FALSE); #imports y_test.txt
+
+# Assign column names to the test data imported for test data
+colnames(subjectTest) = "subjectId";
+colnames(xTest)       = features[,2]; 
+colnames(yTest)       = "activityId";
+
+# Assigin column names to the data imported for training data
 colnames(activityType)  = c('activityId','activityType');
 colnames(subjectTrain)  = "subjectId";
 colnames(xTrain)        = features[,2]; 
@@ -39,17 +49,6 @@ colnames(yTrain)        = "activityId";
 
 # Create the final training set by merging yTrain, subjectTrain, and xTrain
 trainingData = cbind(yTrain,subjectTrain,xTrain);
-
-# Read in the test data
-subjectTest = read.table('./test/subject_test.txt',header=FALSE); #imports subject_test.txt
-xTest       = read.table('./test/x_test.txt',header=FALSE); #imports x_test.txt
-yTest       = read.table('./test/y_test.txt',header=FALSE); #imports y_test.txt
-
-# Assign column names to the test data imported above
-colnames(subjectTest) = "subjectId";
-colnames(xTest)       = features[,2]; 
-colnames(yTest)       = "activityId";
-
 
 # Create the final test set by merging the xTest, yTest and subjectTest data
 testData = cbind(yTest,subjectTest,xTest);
@@ -62,7 +61,7 @@ finalData = rbind(trainingData,testData);
 # to select the desired mean() & stddev() columns
 colNames  = colnames(finalData); 
 
-# 2. Extract only the measurements on the mean and standard deviation for each measurement. 
+# 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
 
 # Create a logicalVector that contains TRUE values for the ID, mean() & stddev() columns and FALSE for others
 logicalVector = (grepl("activity..",colNames) | grepl("subject..",colNames) | grepl("-mean..",colNames) & !grepl("-meanFreq..",colNames) & !grepl("mean..-",colNames) | grepl("-std..",colNames) & !grepl("-std()..-",colNames));
@@ -70,7 +69,7 @@ logicalVector = (grepl("activity..",colNames) | grepl("subject..",colNames) | gr
 # Subset finalData table based on the logicalVector to keep only desired columns
 finalData = finalData[logicalVector==TRUE];
 
-# 3. Use descriptive activity names to name the activities in the data set
+# 3. Uses descriptive activity names to name the activities in the data set
 
 # Merge the finalData set with the acitivityType table to include descriptive activity names
 finalData = merge(finalData,activityType,by='activityId',all.x=TRUE);
@@ -78,7 +77,7 @@ finalData = merge(finalData,activityType,by='activityId',all.x=TRUE);
 # Updating the colNames vector to include the new column names after merge
 colNames  = colnames(finalData); 
 
-# 4. Appropriately label the data set with descriptive activity names. 
+# 4. Appropriately labels the data set with descriptive variable names.
 
 # Cleaning up the variable names
 for (i in 1:length(colNames)) 
@@ -100,7 +99,7 @@ for (i in 1:length(colNames))
 # Reassigning the new descriptive column names to the finalData set
 colnames(finalData) = colNames;
 
-# 5. Create a second, independent tidy data set with the average of each variable for each activity and each subject. 
+# 5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
 # Create a new table, finalDataNoActivityType without the activityType column
 finalDataNoActivityType  = finalData[,names(finalData) != 'activityType'];
